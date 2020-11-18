7230663fc94cdff0ca79c0085e61d58a3179e2b1
Taoyuqing <690230951@qq.com.com>
2020-11-18 10:10:58 +0800

    完成 methodchannel和原生通信 包含了 invoke和result的方式

--- android/app/src/main/java/com/example/usermethodschannel/MainActivity.java
+++ android/app/src/main/java/com/example/usermethodschannel/MainActivity.java
@@ -1,6 +1,69 @@
 package com.example.usermethodschannel;
 
 import io.flutter.embedding.android.FlutterActivity;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "samples.flutter.dev/battery";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        MethodChannel methodChannel =  new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL);
+        methodChannel.setMethodCallHandler(
+                        (call, result) -> {
+                            // Note: this method is invoked on the main thread.
+                            // TODO
+                            if (call.method.equals("getBatteryLevel")) {
+                                int batteryLevel = getBatteryLevel();
+
+                                if (batteryLevel != -1) {
+                                    result.success(batteryLevel);
+                                } else {
+                                    result.error("UNAVAILABLE", "Battery level not available.", null);
+                                }
+                            } if (call.method.equals("makeNativeInvokeFlutter")){
+
+                                Log.d("MainActivety", "configureFlutterEngine: ----------------------------------------------");
+                                Log.d("MainActivety",  String.valueOf(call.arguments));
+                                Log.d("MainActivety", "configureFlutterEngine: ----------------------------------------------");
+                                invokeFlutterMethods(methodChannel,"this is native");
+                            }
+                            else {
+                                result.notImplemented();
+                            }
+                        }
+                );
+    }
+    private int getBatteryLevel() {
+        int batteryLevel = -1;
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            Intent intent = new ContextWrapper(getApplicationContext()).
+                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
+        return batteryLevel;
+    }
+
+    private void invokeFlutterMethods(MethodChannel methodChannel, String arg){
+        methodChannel.invokeMethod("makeNativeInvokeFlutter",arg);
+    }
 }
